1f96b117d875a49dbde5813f8818cef3cba4b24e
Santiago <sanloso25@hotmail.com>
2021-03-10 12:02:29 -0500

    comunicacion front-back

--- src/main/java/com/eci/taskplanner/TaskplannerApplication.java
+++ src/main/java/com/eci/taskplanner/TaskplannerApplication.java
@@ -1,14 +1,30 @@
 package com.eci.taskplanner;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TaskplannerApplication {
+@PropertySource("classpath:application.properties")
+public class TaskplannerApplication implements WebMvcConfigurer{
+
+	@Value( "${cors.urlsValidos}")
+	String corsValidos;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(TaskplannerApplication.class, args);
+	}
+
+	public void addCorsMappings(CorsRegistry registry) {
+
+		String[] origins = corsValidos.split("#");
+		registry.addMapping("/**")
+				.allowedOrigins(origins)
+				.allowedMethods("POST", "GET");
 	}
 
 }
